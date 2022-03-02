cdc3400eae52cb6fde83c80259e11f378f0a58f7
alhabib911 <abdullahalhabib100@gmail.com>
2022-03-02 23:41:36 +0600

    js added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ const allProducts = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch (url)
     .then(res => res.json())
-    .then((data) => showProductsDetails(data.data))
+    .then((data) => {showProductsDetails(data.data)
+        document.getElementById("search-box").value= ""
+    
+    })
     
     
     // console.log(searchValue);
@@ -14,7 +17,7 @@ const allProducts = () => {
 
 const showProductsDetails = (products) => {
     
- for(const product of products){
+ for(const product of products.slice(0, 20)){
     const parents = document.getElementById("all-products")
     const div = document.createElement('div')
     div.innerHTML = `<div>
@@ -41,15 +44,18 @@ const showProductsDetails = (products) => {
 
 
 const details = (info) => {
+    // console.log(info);
     const url = `https://openapi.programming-hero.com/api/phone/${info}`
     fetch (url)
         .then((res) => res.json())
-        .then((data) => savedDetails(data.data))
+        .then((data) => savedDetails(data.data)
+        // console.log(data);
+        )
 }
 
 const savedDetails = (info) => {
 
-    // console.log(info);
+    console.log("info", info);
 
     document.getElementById("details-container").innerHTML = 
     `<div class="left-area ml-12 flex justify-center">
@@ -63,13 +69,13 @@ const savedDetails = (info) => {
         <h4>Brand: ${info.brand}</h4>
         <h4>Chipset:${info.mainFeatures.chipSet}</h4>
         <h4>Display Size: ${info.mainFeatures.displaySize}</h4>
-        <h4>Memory: ${info.mainFeatures.memory}</h4>
+        <h4>Memory: ${info?.mainFeatures.memory}</h4>
         <h4>Storage: ${info.mainFeatures.storage}</h4>
-        <h4>Releage Date: ${info.releaseDate}</h4>
-        <h3 class="font-semibold text-orange-900 text-xl	">Others</h3>
-        <h4>WLAN: ${info.others.WLAN}</h4>
-        <h4>Bluetooth Date: ${info.others.Bluetooth}</h4>
-        <h4>GPS: ${info.others.GPS}</h4>
+        <h4>Releage Date: ${info.releaseDate ? info.releaseDate : "No relase Date found." }</h4>
+        <h3 class="font-semibold text-orange-900 text-xl">Others</h3>
+        <h4>WLAN: ${info?.others?.WLAN ? info.others.WLAN : "No data found"}</h4>
+        <h4>Bluetooth Date: ${info?.others?.Bluetooth ? info.others.Bluetooth : "No data found"}</h4>
+        <h4>GPS: ${info?.others?.GPS ? info.others.GPS: "No data found"}</h4>
         <h4>Sensors: ${info.mainFeatures.sensors}</h4>
         </div>
         </div>
